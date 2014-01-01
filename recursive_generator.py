d3aa31793628ace974d3a36fd60c864df8065e24
Justin Chen <cbjust7@gmail.com>
2014-01-01 23:56:25 +0800

    Parsing nested (using recursion with yield)

---
+++ recursive_generator.py
@@ -0,0 +1,25 @@
+#!/usr/env/python
+#coding:utf-8
+# Author: Justin Chen
+# Date: 2014.1.1
+
+#Note: zh-rCN: 递归生成器，解析多层嵌套
+
+def flatten(nested):
+    '''Parsing nested: recursion with using yield '''
+    try:
+        for sublist in nested:
+            for element in flatten(sublist):
+                yield element
+    except TypeError:
+        yield nested
+
+#help(flatten)
+
+#start test---------------------------------------
+
+nested_one = [[1,2],[3,4],[5], 6, 7]
+nested_two = [[[1],2],3,4,[5, [6, 7]], 8]
+
+print list(flatten(nested_one))
+print list(flatten(nested_two))
