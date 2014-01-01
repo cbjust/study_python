5cfa9216e98d1e5f8d8c9d173be733bd9f9d82ce
Justin Chen <cbjust7@gmail.com>
2014-01-01 22:45:39 +0800

    add fibonacci func

---
+++ fibonacci.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# Author: Justin Chen
+# Date: 2014.01.01
+
+class Fibs:
+    '''print Fibonacci number less than 1000'''
+    def __init__(self):
+        self.a = 0
+        self.b = 1
+    def next(self):
+        self.a, self.b = self.b, self.a + self.b
+        return self.a
+    def __iter__(self):
+        return self
+
+print Fibs.__doc__
+fibs = Fibs()
+print 0,
+for f in fibs:
+    if f > 1000:
+        break
+    print f,
