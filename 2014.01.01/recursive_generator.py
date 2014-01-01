ee90e22b6d9c121091fb1bf8df1a8cfa09b405f2
Justin Chen <cbjust7@gmail.com>
2014-01-02 00:31:44 +0800

    add exception handler to resolve parse element which is a string

--- 2014.01.01/recursive_generator.py
+++ 2014.01.01/recursive_generator.py
@@ -8,6 +8,11 @@
 def flatten(nested):
     '''Parsing nested: recursion with using yield'''
     try:
+        # begin: add exception handler to resolve parse element which is a string
+        try: nested+""
+        except TypeError:pass
+        else:raise TypeError
+        # end
         for sublist in nested:
             for element in flatten(sublist):
                 yield element
@@ -20,6 +25,9 @@ def flatten(nested):
 
 nested_one = [[1,2],[3,4],[5], 6, 7]
 nested_two = [[[1],2],3,4,[5, [6, 7]], 8]
+#type of element is string
+nested_three = ['one', ['two',['three','four']]]
 
 print list(flatten(nested_one))
 print list(flatten(nested_two))
+print list(flatten(nested_three))
